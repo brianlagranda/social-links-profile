93c56eb2e902428d224feba141cd67c5a9a60d2b
Brian Lagranda <brianlagranda@gmail.com>
2024-09-17 14:17:41 -0300

    chore: add personal info and avatar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        tPrimary: 'hsl(0, 0%, 100%)',
-        tSecondary: 'hsl(75, 94%, 57%)',
-        bgButton: 'hsl(0, 0%, 20%)',
-        bgPrimary: 'hsl(0, 0%, 8%)',
-        bgSecondary: 'hsl(0, 0%, 12%)',
+        tPrimary: "hsl(0, 0%, 100%)",
+        tSecondary: "hsl(75, 94%, 57%)",
+        bgButton: "hsl(0, 0%, 20%)",
+        bgPrimary: "hsl(0, 0%, 8%)",
+        bgSecondary: "hsl(0, 0%, 12%)",
       },
       fontFamily: {
-        inter: ['Inter', ...defaultTheme.fontFamily.sans],
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        xs: "375px",
       },
     },
   },
